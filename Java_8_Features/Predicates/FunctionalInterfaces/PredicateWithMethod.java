c1c633ec698c295fb30b82b57b2d3b37a43c116b
Diya Salam <sdiya.salam.github@gmail.com>
2025-02-11 14:57:27 -0500

    add

---
+++ Java_8_Features/Predicates/FunctionalInterfaces/PredicateWithMethod.java
@@ -0,0 +1,16 @@
+package Java_8_Features.Predicates.FunctionalInterfaces;
+
+import java.util.function.Predicate;
+
+public class PredicateWithMethod {
+    public static boolean checkNumber(int num, Predicate<Integer> predicate) {
+        return predicate.test(num);
+    }
+
+    public static void main(String[] args) {
+        Predicate<Integer> isPositive = num -> num > 0;
+
+        System.out.println(checkNumber(5, isPositive));
+        System.out.println(checkNumber(-10, isPositive));
+    }
+}
