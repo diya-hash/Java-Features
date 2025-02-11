e4af77a692bba7fb3177d16732feb1ce72012a56
Diya Salam <sdiya.salam.github@gmail.com>
2025-02-11 15:09:43 -0500

    update

--- Java_8_Features/LambdaExpressions/ICalculator.java
+++ Java_8_Features/LambdaExpressions/ICalculator.java
@@ -1,18 +1,22 @@
 package Java_8_Features.LambdaExpressions;
 
 public class ICalculator {
+    @FunctionalInterface
     public interface IAdd {
         public int add(int a, int b);
     }
 
+    @FunctionalInterface
     interface ICircleArea {
         public double calculateCircleArea(double radius);
     }
 
+    @FunctionalInterface
     interface IDiameter {
         public int calculateDiameter(int radius);
     }
 
+    @FunctionalInterface
     interface IPrint {
         public void printMessage();
     }
