b0584d8c9f5d6e073a7a0b52fb14ed4a82e44fa5
Diya Salam <sdiya.salam.github@gmail.com>
2025-02-11 23:36:19 -0500

    add

---
+++ StreamAPI/StreamExample.java
@@ -0,0 +1,17 @@
+package StreamAPI;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamExample {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("John", "Jane", "Jack", "Jill");
+
+        List<String> namesWithJ = names.stream()
+                .filter(name -> name.startsWith("J"))
+                .map(String::toUpperCase)
+                .collect(Collectors.toList());
+        System.out.println(namesWithJ);
+    }
+}
